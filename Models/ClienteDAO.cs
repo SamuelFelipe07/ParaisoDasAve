d74a2d3b086c78768920b376820269e893ec2d34
Fillipetomazelli <zelirtomazelliro@gmail.com>
2025-11-17 11:58:23 -0400

    quase
    quase certo atualização

--- Models/ClienteDAO.cs
+++ Models/ClienteDAO.cs
@@ -65,7 +65,6 @@ namespace AppExemplo.Models
                             numero = leitor.IsDBNull(leitor.GetOrdinal("numero_cli"))
                                 ? (int?)null
                                 : leitor.GetInt32(leitor.GetOrdinal("numero_cli")),
- 
                     CEP = leitor.IsDBNull(leitor.GetOrdinal("cep_cli")) ? "" : leitor.GetString("cep_cli"),
                     estado = leitor.IsDBNull(leitor.GetOrdinal("estado_cli")) ? "" : leitor.GetString("estado_cli"),
                     cidade = leitor.IsDBNull(leitor.GetOrdinal("cidade_cli")) ? "" : leitor.GetString("cidade_cli")
@@ -76,6 +75,85 @@ namespace AppExemplo.Models
             }
 
             return lista;
+        }
+
+        public Cliente? BuscarPorId(int id)
+        {
+            var comando = _conexao.CreateCommand("SELECT * FROM cliente WHERE id_cli = @id;");
+            comando.Parameters.AddWithValue("@id", id);
+
+            var leitor = comando.ExecuteReader();
+
+            if (leitor.Read())
+            {
+                var cliente = new Cliente
+                {
+                    id = leitor.GetInt32("id_cli"),
+                    Nome = leitor.GetString("nome_cli"),
+                    CPF = leitor.IsDBNull(leitor.GetOrdinal("cpf_cli")) ? "" : leitor.GetString("cpf_cli"),
+                    dataNascimento = leitor.IsDBNull(leitor.GetOrdinal("data_nascimento_cli")) ? "" : leitor.GetString("data_nascimento_cli"),
+                    RG = leitor.IsDBNull(leitor.GetOrdinal("rg_cli")) ? "" : leitor.GetString("rg_cli"),
+                    bairro = leitor.IsDBNull(leitor.GetOrdinal("bairro_cli")) ? "" : leitor.GetString("bairro_cli"),
+                    rua = leitor.IsDBNull(leitor.GetOrdinal("rua_cli")) ? "" : leitor.GetString("rua_cli"),
+
+                            numero = leitor.IsDBNull(leitor.GetOrdinal("numero_cli"))
+                                ? (int?)null
+                                : leitor.GetInt32(leitor.GetOrdinal("numero_cli")),
+                    CEP = leitor.IsDBNull(leitor.GetOrdinal("cep_cli")) ? "" : leitor.GetString("cep_cli"),
+                    estado = leitor.IsDBNull(leitor.GetOrdinal("estado_cli")) ? "" : leitor.GetString("estado_cli"),
+                    cidade = leitor.IsDBNull(leitor.GetOrdinal("cidade_cli")) ? "" : leitor.GetString("cidade_cli")
+
+                };
+
+                return cliente;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public void Atualizar(Cliente cliente)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand(
+                "UPDATE cliente SET nome_cli = @_Nome, cpf_cli = @_CPF, data_nascimento_cli = @_dataNascimento, rg_cli = @_RG, bairro_cli = @_bairro, rua_cli = @_rua, numero_cli = @_numero, cep_cli = @_CEP, estado_cli = @_estado, cidade_cli = @_cidade WHERE id_cli = @_id;");
+
+
+                comando.Parameters.AddWithValue("@_nome_cli", cliente.Nome);
+                comando.Parameters.AddWithValue("@_cpf_cli", cliente.CPF);
+                comando.Parameters.AddWithValue("@_data_nascimento_cli", cliente.dataNascimento);
+                comando.Parameters.AddWithValue("@_rg_cli", cliente.RG);
+                comando.Parameters.AddWithValue("@_bairro_cli", cliente.bairro);
+                comando.Parameters.AddWithValue("@_rua_cli", cliente.rua);
+                comando.Parameters.AddWithValue("@_numero_cli", cliente.numero);
+                comando.Parameters.AddWithValue("@_cep_cli", cliente.CEP);
+                comando.Parameters.AddWithValue("@_estado_cli", cliente.estado);
+                comando.Parameters.AddWithValue("@_cidade_cli", cliente.cidade);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            try
+            {
+                var comando = _conexao.CreateCommand("DELETE FROM cliente WHERE id_cli = @id;");
+
+                comando.Parameters.AddWithValue("@id", id);
+
+                comando.ExecuteNonQuery();
+            }
+            catch
+            {
+                throw;
+            }
         }
 
     }
